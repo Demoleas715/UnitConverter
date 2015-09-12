e5f5534202d1ec49830ce6f874a3220a8f3d7088
Demoleas715 <Demoleas715@gmail.com>
2015-09-12 19:33:32 -0400

    Kilos to pounds

--- UnitConverter.py
+++ UnitConverter.py
@@ -2,8 +2,15 @@
 
 def pound_kilo(pounds):
 	convertion_rate = 0.453592
-	kilo = pounds * convertion_rate
-	return kilo
+	kilos = pounds * convertion_rate
+	return kilos
+
+def kilo_pound(kilos):
+	convertion_rate = 2.20462
+	pounds = kilos * convertion_rate
+	return pounds
 
 pounds = int(input("Enter Pounds: "))
 print (pound_kilo(pounds))
+kilos = int(input("Enter Kilos: "))
+print (kilo_pound(kilos))
