de0d58188daa7e166b0d5c879661b9a61a964084
demoleas21 <andrew.demoleas@gmail.com>
2015-09-12 21:33:51 -0400

    Protected Modules

--- UnitConverter.py
+++ UnitConverter.py
@@ -1,20 +1,21 @@
 """ Unit Converter """
 
 def pound_kilo(pounds):
-	convertion_rate = 0.453592
-	kilos = pounds * convertion_rate
-	return kilos
+    convertion_rate = 0.453592
+    kilos = pounds * convertion_rate
+    return kilos
 
 def kilo_pound(kilos):
-	convertion_rate = 2.20462
-	pounds = kilos * convertion_rate
-	return pounds
+    convertion_rate = 2.20462
+    pounds = kilos * convertion_rate
+    return pounds
 
-print ("\n")
-pounds = int(input("Enter Pounds: "))
-print ("\n")
-print (pound_kilo(pounds))
-print ("\n\n")
-kilos = int(input("Enter Kilos: "))
-print ("\n")
-print (kilo_pound(kilos))
+if __name__ == '__main__':
+    print ("\n")
+    pounds = int(input("Enter Pounds: "))
+    print ("\n")
+    print (pound_kilo(pounds))
+    print ("\n\n")
+    kilos = int(input("Enter Kilos: "))
+    print ("\n")
+    print (kilo_pound(kilos))
